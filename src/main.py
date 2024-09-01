5fac27e60bba53632c361c53a3a02013571e86f3
Егор Парфинцов <egor.porf1@mail.ru>
2024-09-01 21:48:37 +0300

    создание компонентов и запуск приложения

--- src/main.py
+++ src/main.py
@@ -21,15 +21,14 @@ if __name__ == '__main__':
         'ф-я Химмельблау': Himmelblau
     }
     
-    App(
-        ThemedTk(theme='plastik'),
+    ThemedTkApp(
         {
             'ЛР1': GD_AlgorithmFrame(gd_functions, txt, plt),
             'ЛР3': GeneticAlgorithmFrame(functions, txt, plt),
             'ЛР4': PSO_AlgorithmFrame(functions, txt, plt),
             'ЛР5': BeeAlgorithmFrame(functions, txt, plt)
         },
-        txt,
-        plt
-    )
+        plt,
+        txt
+    ).run()
 
