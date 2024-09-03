e325a878c769aad980dea6102ff47570e03727a4
Егор Парфинцов <egor.porf1@mail.ru>
2024-09-03 15:08:51 +0300

    упрощенное создание приложения

--- src/main.py
+++ src/main.py
@@ -1,34 +1,10 @@
-from tkinter import Tk
-
-from ttkthemes import ThemedTk
-
 from app import *
 from funcs import *
+import app_builder as ab
 
 
 if __name__ == '__main__':
-    txt: TextWidget = TextFrame()
-    plt: PlotWidget = PlotFrame()
+    themed_tk_builder = ab.ThemedTkAppBuilder()
+    builder_director = ab.AppBuilderDirector()
+    builder_director.maximum_app(themed_tk_builder).run()
     
-    gd_functions = {
-        'пример 3.1': example_3_1,
-    }
-    
-    functions = {
-        'ф-я сферы': sphere,
-        'ф-я Растригина': Rastrigin,
-        'ф-я Розенброка': Rosenbrock,
-        'ф-я Химмельблау': Himmelblau
-    }
-    
-    ThemedTkApp(
-        {
-            'ЛР1': GD_AlgorithmFrame(gd_functions, txt, plt),
-            'ЛР3': GeneticAlgorithmFrame(functions, txt, plt),
-            'ЛР4': PSO_AlgorithmFrame(functions, txt, plt),
-            'ЛР5': BeeAlgorithmFrame(functions, txt, plt)
-        },
-        plt,
-        txt
-    ).run()
-
