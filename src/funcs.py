a2b49a1a9a188f799b2be599f2bf2bf7f0d725c4
Егор Парфинцов <egor.porf1@mail.ru>
2024-09-04 00:11:46 +0300

    переписал функции

--- src/funcs.py
+++ src/funcs.py
@@ -10,39 +10,30 @@ __all__ = [
     'Gomez_Levy'
 ]
 
-from typing import Iterable
 import numpy as np
 
-def _example_3_1_func(x: Iterable[float]) -> float:
-    x1, x2, *_ = x
+def _example_3_1_func(x1: float, x2: float) -> float:
     return 2 * x1 * x1 + x1 * x2 + x2 * x2
 
-def _example_3_1_grad(x: Iterable[float]) -> Iterable[float]:
-    x1, x2, *_ = x
+def _example_3_1_grad(x1: float, x2: float) -> tuple[float, float]:
     return [4 * x1 + x2, x1 + 2 * x2]
 
 example_3_1 = (_example_3_1_func, _example_3_1_grad)
 
-def Rosenbrock(x: Iterable[float]) -> float:
-    x1, x2, *_ = x
+def Rosenbrock(x1: float, x2: float) -> float:
     return (1 - x1) ** 2 + 100 * (x2 - x1 ** 2) ** 2
 
-def sphere(x: Iterable[float]) -> float:
-    x1, x2, *_ = x
+def sphere(x1: float, x2: float) -> float:
     return x1 ** 2 + x2 ** 2
 
-def Rastrigin(x: Iterable[float]) -> float:
-    x1, x2, *_ = x
+def Rastrigin(x1: float, x2: float) -> float:
     return 20 + (x1**2 - 10*np.cos(2*np.pi*x1)) + (x2**2 - 10*np.cos(2*np.pi*x2))
 
-def Himmelblau(x: Iterable[float]) -> float:
-    x1, x2, *_ = x
+def Himmelblau(x1: float, x2: float) -> float:
     return (x1**2 + x2 - 11)**2 + (x1 + x2**2 - 7)**2
 
-def Simionescu(x: Iterable[float]) -> float:
-    x1, x2, *_ = x
+def Simionescu(x1: float, x2: float) -> float:
     return 0.1 * x1 * x2
 
-def Gomez_Levy(x: Iterable[float]) -> float:
-    x1, x2, *_ = x
-    return 4*x1**2 - 2.1*x**4 + 1/3*x**6 + x1*x2 - 4*x2**2 + 4*x2**4
+def Gomez_Levy(x1: float, x2: float) -> float:
+    return 4*x1**2 - 2.1*x1**4 + 1/3*x1**6 + x1*x2 - 4*x2**2 + 4*x2**4
